9e3ed002cb730acd17cbd1e179c8d96805254879
rzounlom <rzounlom@gmail.com>
2025-04-08 19:51:09 -0400

    add example for users

--- src/pages/FetchExample1.tsx
+++ src/pages/FetchExample1.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Spinner, Table } from "react-bootstrap";
 
+import { Link } from "react-router-dom";
+
 const FetchExample1: React.FC = () => {
   const [data, setData] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
@@ -9,7 +11,7 @@ const FetchExample1: React.FC = () => {
     const fetchData = async () => {
       try {
         const response = await fetch(
-          "https://jsonplaceholder.typicode.com/posts"
+          "https://jsonplaceholder.typicode.com/users"
         );
         const result = await response.json();
         setData(result.slice(0, 5)); // Get only 5 items for display
@@ -31,14 +33,17 @@ const FetchExample1: React.FC = () => {
         <thead>
           <tr>
             <th>ID</th>
-            <th>Title</th>
+            <th>Name</th>
           </tr>
         </thead>
         <tbody>
           {data.map((item) => (
             <tr key={item.id}>
               <td>{item.id}</td>
-              <td>{item.title}</td>
+
+              <td>
+                <Link to={`/user/${item.id}`}>{item.name}</Link>
+              </td>
             </tr>
           ))}
         </tbody>
